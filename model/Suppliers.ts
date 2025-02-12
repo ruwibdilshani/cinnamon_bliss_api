a71f2b0efe552b4ef7fa0a9d1ef954e67361a51d
ruwibdilshani <ruwibdilshani0921@gmail.com>
2025-02-12 12:25:35 +0530

    Supplie .ts done

---
+++ model/Suppliers.ts
@@ -0,0 +1,12 @@
+
+export default class Supplier {
+    supplierId! : string;
+    firstName! : string;
+    lastName! : string;
+    gender! : string;
+    addressLine1!: string;
+    postcode!: string;
+    contactNo!: string;
+    email!: string;
+
+}
