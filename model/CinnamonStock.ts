41eb4cb86ea02506e75f77ad12425b07ced118d6
ruwibdilshani <ruwibdilshani0921@gmail.com>
2025-02-12 12:31:05 +0530

    CinnamonStock.ts done

---
+++ model/CinnamonStock.ts
@@ -0,0 +1,9 @@
+
+export default class CinnamonStock {
+    stockId!: string;
+    batchCode!: string;
+    type!: string;
+    quantity!: number;
+    supplierId!: string;
+    receivedDate!: Date | string;
+}
