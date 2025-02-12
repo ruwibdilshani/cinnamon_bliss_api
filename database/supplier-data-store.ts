f4a2f4622162a3aea76da48c15489fed408bc02d
ruwibdilshani <ruwibdilshani0921@gmail.com>
2025-02-12 21:00:13 +0530

    supplier-data-store.ts ok

---
+++ database/supplier-data-store.ts
@@ -0,0 +1,81 @@
+import {PrismaClient} from "@prisma/client";
+import Supplier from "../model/Suppliers";
+
+const prisma = new PrismaClient();
+
+export async function addSupplier(supplier:Supplier){
+    try{
+        return await prisma.suppliers.create({
+            data : {
+                supplierID : supplier.supplierID,
+                firstName :supplier.firstName,
+                lastName :supplier.lastName,
+                gender :supplier.gender,
+                addressLine01 :supplier.addressLine1,
+                postalCode :supplier.postcode,
+                contactNo : supplier.contactNo,
+                email : supplier.email
+            }
+        });
+    }catch(e){
+        console.error(`Error adding supplier: ${e}`);
+        throw e;
+    }
+}
+
+export async function deleteSupplier(id: string) {
+    try {
+        return await prisma.suppliers.delete({
+            where:{
+                supplierID: id
+            }
+        });
+    } catch (error) {
+        console.error(`Error deleting supplier: ${error}`);
+        throw error;
+    }
+}
+
+export async function updateSupplier(id: string, supplier: Supplier ) {
+    try {
+        return await prisma.suppliers.update({
+            where:{
+            supplierID: id
+        },
+            data:{
+                firstName :supplier.firstName,
+                lastName :supplier.lastName,
+                gender :supplier.gender,
+                addressLine01 :supplier.addressLine1,
+                postalCode :supplier.postcode,
+                contactNo : supplier.contactNo,
+                email : supplier.email
+            }
+            });
+        }catch(e){
+        console.error(`Error updating supplier: ${e}`);
+        throw e;
+    }
+}
+
+export async function getSuppliers() {
+    try {
+        return await prisma.suppliers.findMany();
+    } catch (error) {
+        console.error(`Error getting all supplier: ${error}`);
+        throw error;
+    }
+}
+
+export async function getSupplier(id: string) {
+    try {
+        return await prisma.suppliers.findUnique({
+            where: {
+                supplierID: id
+            }
+        });
+    } catch (error) {
+        console.error(`Error getting employee: ${error}`);
+        throw error;
+    }
+}
