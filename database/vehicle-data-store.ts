338f4dae9be67fd3d1df38582fb976b11d9dd405
ruwibdilshani <ruwibdilshani0921@gmail.com>
2025-02-16 22:23:06 +0530

    vehicale-data-store done

---
+++ database/vehicle-data-store.ts
@@ -0,0 +1,83 @@
+
+import Vehicle from "../model/Vehicle";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function addVehicle(vehicle:Vehicle) {
+    try {
+        return await  prisma.vehicles.create({
+            data: {
+                vehicleID:vehicle.vehicleID,
+                licensePlate:vehicle.licensePlate,
+                model:vehicle.model,
+                capacity:vehicle.capacity,
+                employeeID:vehicle.employeeID,
+
+            }
+        });
+    }catch (error) {
+        console.error(`Error adding Vehicle: ${error}`);
+        throw error;
+    }
+}
+
+export async function deleteVehicle(id: string) {
+    try{
+        return await  prisma.vehicles.delete({
+            where:{
+                licensePlate:id,
+            }
+        });
+    }catch (error) {
+        console.error(`Error deleting vehicle: ${error}`);
+        throw error;
+    }
+}
+
+export async function updateVehicle(id: string, vehicle: Vehicle) {
+    try{
+        return await prisma.vehicles.update({
+            where: {
+                licensePlate: id
+            },
+            data: {
+                vehicleID: vehicle.vehicleID,
+                licensePlate: vehicle.licensePlate,
+                model: vehicle.model,
+                capacity: vehicle.capacity,
+                employeeID: vehicle.employeeID
+            }
+        });
+    }catch (error) {
+        console.error(`Error updating vehicle: ${error}`);
+        throw error;
+    }
+}
+
+export async function getVehicles() {
+    try{
+        return await prisma.vehicles.findMany();
+
+    }catch (error) {
+        console.error(`Error getting vehicles: ${error}`);
+        throw error;
+    }
+}
+
+export async function getVehicleById(id: string) {
+    try {
+        return await prisma.vehicles.findUnique({
+            where: {
+                licensePlate: id
+            }
+        });
+    } catch (error) {
+        console.error(`Error getting vehicle by id: ${error}`);
+        throw error;
+    }
+}
+
+
+
+
