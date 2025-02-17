7011e8bb7a0c651de060903c2f7001c8d45051b9
ruwibdilshani <ruwibdilshani0921@gmail.com>
2025-02-17 09:23:02 +0530

    server ts done

--- server.ts
+++ server.ts
@@ -1,4 +1,10 @@
 import express from "express";
+import employeeRoutes  from "./routes/employee-routes";
+import supplierRoutes from "./routes/supplier-routes";
+import userAdminRoutes from "./routes/userAdmin-routes";
+import cinnamonStockRoutes from "./routes/cinnamonStock-routes";
+import logRoutes from "./routes/log-routes";
+import vehicleRoutes from "./routes/vehicle-routes";
 
 const app = express();
 
@@ -11,6 +17,18 @@ app.use('/',(req,res,next)=>{
 })
 
 app.use(express.json());
+
+app.use('/employees', employeeRoutes);
+
+app.use('/suppliers',supplierRoutes);
+
+app.use('/userAdmin',userAdminRoutes);
+
+app.use('/cinnamonStock',cinnamonStockRoutes);
+
+app.use('/log',logRoutes);
+
+app.use('/vehicle',vehicleRoutes)
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
